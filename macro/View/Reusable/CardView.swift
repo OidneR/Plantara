2da80ae4e8428e14cba94aebc18d5492a36a2d3a
TuanMudaDion <lamilgadion36@gmail.com>
2021-11-01 14:39:00 +0700

    Searchbar Belom bisa

---
+++ macro/View/Reusable/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  macro
+//
+//  Created by Dion Lamilga on 01/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct CardView: View{
+    
+    @Binding var viewModel: [Tanaman]
+    @Binding var Grid : [Int]
+    
+    var body: some View{
+        VStack{
+            if !self.Grid.isEmpty{
+                ScrollView(.vertical, showsIndicators: false) {
+                    
+                    VStack(spacing : 25){
+                        ForEach(self.Grid,id: \.self){i in
+                            
+                            HStack(spacing: 15){
+                                ForEach(i...i+1,id: \.self){j in
+                                    
+                                    VStack{
+                                        if j != self.viewModel.count{
+                                            CardViewReusable(dataTanaman: self.viewModel[j])
+                                        }
+                                    }
+                                }
+                                if i == self.Grid.last && self.viewModel.count % 2 != 0 {
+                                    Spacer(minLength: 0)
+                                }
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+}
