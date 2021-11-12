1018942dd110e39c2029bf5a93e771944af21c04
vincent-chandra <vincentchandra1@gmail.com>
2021-11-12 22:02:02 +0700

    fix DeviceDetail (iPad) when keyboard appear

---
+++ macro/Helper/KeyboardHeightHelper.swift
@@ -0,0 +1,29 @@
+//
+//  KeyboardHeightHelper.swift
+//  macro
+//
+//  Created by Vincent on 11/11/21.
+//
+
+import UIKit
+import Foundation
+
+class KeyboardHeightHelper: ObservableObject{
+    @Published var keyboardHeight: CGFloat = 0
+    
+    init(){
+        self.listenForKeyboardNotifications()
+    }
+    
+    private func listenForKeyboardNotifications(){
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: .main) { (notification) in
+            guard let userInfo = notification.userInfo,
+                  let keyboardRect = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+            self.keyboardHeight = keyboardRect.height - 80
+        }
+        
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidHideNotification, object: nil, queue: .main) { (notification) in
+            self.keyboardHeight = 0
+        }
+    }
+}
